d16a83bd99ffb601b1491f690c9011149185cc2d
Adrian Borucki <gentoolx@gmail.com>
2017-09-02 17:52:12 +0200

    Make the install script create needed directories

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir -p ~/.local/share/jupyter/kernels/guile-scheme
 sed "s|{env:HOME}|$HOME|" kernel.json > ~/.local/share/jupyter/kernels/guile-scheme/kernel.json
 cp src/load.scm ~/.local/share/jupyter/kernels/guile-scheme
 cp -r src/guile-jupyter ~/.local/share/jupyter/kernels/guile-scheme
